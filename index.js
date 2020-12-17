c00404a2bf3dc6f666d08e5101ef7650d80efd21
Bryce B <bryce.boddy@gmail.com>
2020-12-17 15:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let main = document.getElementById("main");
 main.remove();
 
 let newHeader = document.createElement('H1');
+newHeader.setAttribute("id", "victory");
 newHeader.innerHTML = "YOUR-NAME"
